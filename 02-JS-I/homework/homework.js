3471f5a14ab7e3c66957f9905b31cb6560994e0c
Santiago Francisco Amado Sopo <sfrestrepo01qgmail.com>
2021-09-28 13:21:37 -0500

    changes

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -72,8 +72,13 @@ function sonIguales(x, y) {
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
-  // Tu código:
-  
+  // Tu código: 
+  if (x === y) {
+    return true;
+  } else {
+    return false;
+  }
+}
 }
 
 function menosQueNoventa(num) {
