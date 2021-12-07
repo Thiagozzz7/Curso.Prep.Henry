ef5403e53ceb7483b5edd52b96a526a1f2c6a694
Thiagozzz <sfrestrepo01@gmail.com>
2021-12-07 12:09:21 -0500

    changes

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -181,40 +181,47 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  var newString = str + "!";
+  return newString;
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  var fullName = nombre + ' ' + apellido;
+  return fullName;
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  
+  var saludo = 'Hola ' + nombre + '!';
+  return saludo;
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
-  
+  var arearect = alto * ancho;
+  return arearect;
 }
 
 
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var perímetro = lado * 4;
+  return perímetro;
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+  var areatrian = (base * altura) / 2;
+  return areatrian; 
 }
 
 
@@ -222,7 +229,9 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  //return euro * 1.20;
+  var cambio = euro * 1.20;
+  return cambio;
 }
 
 
@@ -232,10 +241,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra.length === 1){
+        if (letra === 'A' || 'E' || 'I' || 'O' || 'U' || 'a' || 'e' || 'i' || 'o' || 'u') {
+          return "Es vocal"
+    } return "Dato incorrecto"
+  } else {
+    return "Dato incorrecto"
+  }  
 }
-
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
