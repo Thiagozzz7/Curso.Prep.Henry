51d4d9d6f93dbb43803cf95b6e704f32955c2214
Thiagozzz <sfrestrepo01@gmail.com>
2021-12-17 17:07:32 -0500

    JVX

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,29 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+  /* let p=Object.getOwnPropertyNames(objeto);
+  let newArray = [];
+
+  for (let i = 0; i < p.length; i++) {
+    newArray.push([p[i], objeto[p[i]]]);  
+  }
+  return newArray; */
+
+
+ /*  var matriz = []
+  for(let propiedad in objeto){
+    var arr = []
+    arr.push(propiedad)
+    arr.push(objeto[propiedad])
+    matriz.push(arr)
+  }
+  return matriz; */
+
+var matriz = [];
+  for(let propiedad in objeto){
+    matriz.push([propiedad, objeto[propiedad]])
+  }
+  return matriz;
 }
 
 
@@ -18,6 +41,16 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj = {}
+
+  for ( i=0; i<string.length; i++){
+    if(!obj[string[i]]) {
+      obj[string[i]] = 1;
+    } else {
+      obj[string[i]]++
+    }
+  }
+  return obj;
 }
 
 
